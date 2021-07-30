ec839e3bc03c3d308d5d4587844157116e0705ae
dccakar <88180400+dccakar@users.noreply.github.com>
2021-07-30 09:56:14 +0300

    Add files via upload

---
+++ src/Acme.BookStore.Application.Contracts/Books/IBookAppService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.BookStore.Books
+{
+    public interface IBookAppService :
+        ICrudAppService< //Defines CRUD methods
+            BookDto, //Used to show books
+            Guid, //Primary key of the book entity
+            PagedAndSortedResultRequestDto, //Used for paging/sorting
+            CreateUpdateBookDto>
+    {
+        Task<ListResultDto<AuthorLookupDto>> GetAuthorLookupAsync();
+        Task<PagedResultDto<BookDto>> GetFavouriteListAsync(PagedAndSortedResultRequestDto input);
+
+        Task LikeAsync(Guid id);
+        Task DislikeAsync(Guid id);
+    }
+}
---
+++ src/Acme.BookStore.Application.Contracts/FavouriteBooks/FavouriteBooksDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Books
+{
+    public class FavouriteBooksDto 
+    {
+        public string Name { get; set; }
+
+        public BookType Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+        public Guid AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public bool IsFavourite { get; set; }
+    }
+}

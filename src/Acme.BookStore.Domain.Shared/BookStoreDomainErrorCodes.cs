ac67093de73802c8370a5bbada777f1cb7654bd6
dccakar <88180400+dccakar@users.noreply.github.com>
2021-08-05 09:22:41 +0300

    Update BookStoreDomainErrorCodes.cs

--- src/Acme.BookStore.Domain.Shared/BookStoreDomainErrorCodes.cs
+++ src/Acme.BookStore.Domain.Shared/BookStoreDomainErrorCodes.cs
@@ -1,8 +1,10 @@
-﻿namespace Acme.BookStore
+namespace Acme.BookStore
 {
     public static class BookStoreDomainErrorCodes
     {
         /* You can add your business exception error codes here, as constants */
         public const string AuthorAlreadyExists = "BookStore:00001";
+        public const string BookIsAlreadyLiked = "BookStore:00002";
+        public const string AuthorAlreadyLiked = "BookStore:00003";
     }
 }
